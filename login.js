f0ee52df5b64a6c6ea0a39e799f4dc7125e17377
Khushi Rathi <khushi.219309030@muj.manipal.edu>
2023-11-22 14:53:05 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,47 @@
+//setting up firebase with our website
+const firebaseApp = firebase.initializeApp({ 
+    apiKey: "AIzaSyA14ANjQxKbHcxiaFj1mUxl_SAd-FkTaFc",
+  authDomain: "login-form-a3677.firebaseapp.com",
+  projectId: "login-form-a3677",
+  storageBucket: "login-form-a3677.appspot.com",
+  messagingSenderId: "253888632013",
+  appId: "1:253888632013:web:4f12fcb904c0b6e0ce4584",
+  measurementId: "G-JDPSEKMBN2"
+ });
+   const db = firebaseApp.firestore();
+   const auth = firebaseApp.auth();
+
+   //SignUP function
+   const signUp=()=>{
+    const email= document.getElementById("email").value;
+    const password= document.getElementById("password").value;;
+    console.log(email, password);
+    //firebase code
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((result) => {
+    // Signed in 
+    alert("You are signed up");
+    console.log(result);
+    // ...
+  })
+  .catch((error) => {
+    console.log(error.code);
+    console.log(error.message);
+    // ..
+  });
+   }
+
+
+   //Login fiunction
+   const signIn=()=>{
+    firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((result) => {
+    alert("You are signed in... Welcome to CraveHub.com, Happy Munching!")
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error.code);
+    console.log(error.message);
+  });
+
+   }
